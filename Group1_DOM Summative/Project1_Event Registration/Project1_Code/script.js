e4cc283d65d45556ddcbac1420d9e4b5c32394cf
jophielMMCM <jtRodriguez@mcm.edu.ph>
2026-03-06 22:13:42 +0800

    Update script.js

--- Group1_DOM Summative/Project1_Event Registration/Project1_Code/script.js
+++ Group1_DOM Summative/Project1_Event Registration/Project1_Code/script.js
@@ -1,4 +1,10 @@
-//Code for event registration
+//Code for event registration error messages
+//"Full name is required."
+//"Enter a valid email address."
+//"Please select and event"
+//Register button should gray-ou/be disabled if all the forms are not filled
+
+//default state: "No attendees registered yet"
 
 //Code for random ID generator
 
