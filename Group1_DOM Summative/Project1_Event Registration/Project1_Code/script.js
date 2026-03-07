6d5ceafc2e9728c62d2585cc85741a780155f7a8
Teehooo <maMontano@mcm.edu.ph>
2026-03-07 21:55:55 +0800

    Update script.js

--- Group1_DOM Summative/Project1_Event Registration/Project1_Code/script.js
+++ Group1_DOM Summative/Project1_Event Registration/Project1_Code/script.js
@@ -1,5 +1,6 @@
 //Code for event registration error messages
 //"Full name is required."
+const nameInput = document.getElementById("Full name");
 //"Enter a valid email address."
 //"Please select and event"
 //Register button should grayed-out/disabled if all the forms are not filled
@@ -9,4 +10,5 @@
 //Code for random ID generator
 
 //Code for filter by event
+
 
